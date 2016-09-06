6b31b8a42d7018b524a532259088917a79bc1c5c
Sean Esteva <sean.esteva@kiosked.com>
2016-09-06 01:55:16 -0700

    added className to the <li> rendered in list-item.jsx to allow toggle between active and "", also added || statement to <Button> component to search in props for an itemTitle and if it cant find one it displays the original title provided in app.jsx options object. Last thing, added ternerary to <ListItem className to toggle between item active or empty string

--- src/dropdown.jsx
+++ src/dropdown.jsx
@@ -14,18 +14,25 @@ module.exports = React.createClass({
     return { open: false }
   },
   handleItemClick: function (item) {
-    console.log(item);
+    this.setState({
+      open: false,
+      itemTitle: item
+    });
   },
   render: function() {
     var list = this.props.items.map(function(item){
-      return <ListItem item={item} whenItemClicked={this.handleItemClick}/>
+      return <ListItem
+              item={item}
+              whenItemClicked={this.handleItemClick}
+              className={this.state.itemTitle === item ? "active" : "" }
+              />
     }.bind(this));
 
     return <div className="dropdown">
       <Button
         whenClicked={this.handleClick}
         className="btn-default"
-        title={this.props.title}
+        title={this.state.itemTitle || this.props.title}
         subTitleClassName="caret"
         />
       <ul className={"dropdown-menu " + (this.state.open ? "show" : "") }>
--- src/list-item.jsx
+++ src/list-item.jsx
@@ -5,6 +5,8 @@ module.exports = React.createClass({
     this.props.whenItemClicked(this.props.item);
   },
   render: function () {
-    return <li><a onClick={ this.handleClick }>{this.props.item}</a></li>
+    return <li className={this.props.className}>
+      <a onClick={ this.handleClick }>{this.props.item}</a>
+    </li>
   }
 });
